1e0811a7a78f04c59f9c68197d2827cb1f558b8f
Bryant Biggs <bryantbiggs@gmail.com>
2022-11-07 09:47:44 -0500

    chore: Updates based on security review feedback

--- examples/complete/main.tf
+++ examples/complete/main.tf
@@ -192,11 +192,13 @@ module "irsa" {
 }
 
 resource "aws_security_group" "vpc_link_sg" {
+  # checkov:skip=CKV2_AWS_5
   name        = "${local.name}-vpc-link"
   description = "Security group for API Gateway v2 VPC link"
   vpc_id      = module.vpc.vpc_id
 
   ingress {
+    description = "Ingress all from VPC"
     from_port   = 0
     to_port     = 0
     protocol    = "-1"
@@ -204,6 +206,7 @@ resource "aws_security_group" "vpc_link_sg" {
   }
 
   egress {
+    description = "Egress all to VPC"
     from_port   = 0
     to_port     = 0
     protocol    = "-1"
