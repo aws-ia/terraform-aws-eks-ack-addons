2986ec6969503b9588ce5569040d2d1063292136
Gu <victorgw@amazon.com>
2022-12-14 12:55:55 -0600

    add ecr login

--- examples/complete/main.tf
+++ examples/complete/main.tf
@@ -20,7 +20,18 @@ data "aws_eks_cluster_auth" "this" {
   name = module.eks_blueprints.eks_cluster_id
 }
 
+# Equivalent of aws ecr get-login
 data "aws_ecr_authorization_token" "token" {}
+# Multiple docker push commands can be run against a single token
+resource "null_resource" "renew_ecr_token" {
+  triggers = {
+    token_expired = data.aws_ecr_authorization_token.ecr_token.expires_at
+  }
+
+  provisioner "local-exec" {
+    command = "echo ${data.aws_ecr_authorization_token.ecr_token.password} | docker login --username ${data.aws_ecr_authorization_token.ecr_token.user_name} --password-stdin ${data.aws_ecr_authorization_token.ecr_token.proxy_endpoint}"
+  }
+}
 
 data "aws_availability_zones" "available" {}
 data "aws_caller_identity" "current" {}
