d220eaccb0b2a343fd3d0d73c7e9d78b43ab7fee
Gu <victorgw@amazon.com>
2023-01-09 11:59:44 -0600

    avoid duplicate emrcontainer policy

--- main.tf
+++ main.tf
@@ -447,7 +447,6 @@ module "emrcontainers" {
 resource "aws_iam_policy" "emrcontainers" {
   count = var.enable_emrcontainers ? 1 : 0
 
-  name        = format("%s-%s", local.emr_name, "controller-iam-policies")
   description = "IAM policy for EMRcontainers controller"
   path        = "/"
   policy      = data.aws_iam_policy_document.emrcontainers.json
