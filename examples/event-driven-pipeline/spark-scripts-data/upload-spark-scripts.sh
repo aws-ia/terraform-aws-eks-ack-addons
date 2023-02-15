6d572a6dc2ab5edb13798d662c60b80477a51861
Gu <victorgw@amazon.com>
2023-02-15 10:54:59 -0600

    remove event pipeline code

--- examples/event-driven-pipeline/spark-scripts-data/upload-spark-scripts.sh
+++
@@ -1,8 +0,0 @@
-
-
-S3_BUCKET="sparkjob-demo-bucket"
-REGION="us-west-2"       # Enter region
-
-
-# Copy PySpark Script  to S3 bucket
-aws s3 cp spark-scripts-data/pyspark-taxi-trip.py s3://${S3_BUCKET}/scripts/ --region ${REGION}
